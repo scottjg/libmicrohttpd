f1ea3fc6ed1521579051a261f48479a9a660c96d
Scott J. Goldman <scottjg@github.com>
2015-07-08 11:59:00 -0700

    try this instead, i don't think it's working on linux right now
    cc @simonsj

--- src/include/platform_interface.h
+++ src/include/platform_interface.h
@@ -127,7 +127,7 @@
 /* MHD_pipe_ create pipe (!MHD_DONT_USE_PIPES) /
  *           create two connected sockets (MHD_DONT_USE_PIPES) */
 #ifndef MHD_DONT_USE_PIPES
-#ifdef pipe2
+#ifdef __linux__
 #define MHD_pipe_(fdarr) pipe2((fdarr), O_CLOEXEC)
 #else
 #define MHD_pipe_(fdarr) pipe((fdarr))
